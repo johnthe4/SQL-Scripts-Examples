85c8dd52b2d323284da730865f96e3171056cc8d
John McGaughey <JMcGaughey@tql.com>
2022-04-27 15:01:52 -0400

    updated notes practice EdDb

--- practice-query-EdDb.sql
+++ practice-query-EdDb.sql
@@ -11,3 +11,18 @@ select * from Student
 select Lastname, Firstname, YearsExperience from Instructor
 	order by Lastname;
 
+-- join will not bring students with null foreign key 
+select s.Lastname as 'Students Last Name', m.Description as 'Major'
+	from Student s
+	join Major m
+		on s.MajorId = m.Id;
+
+-- joining more than 2 tables
+-- 3rd table can join on either 1st or 2nd
+select s.Lastname as 'Name', c.Subject, c.Code from Student s
+	join StudentClass sC
+		on sc.StudentId = s.Id
+	join Class c
+		on sc.ClassId = c.Id
+	where s.Lastname = 'Ant';
+		
