bae1bacf14150571eab337c54988fabc74c39463
John McGaughey <JMcGaughey@tql.com>
2022-04-27 11:30:22 -0400

    additions to practice query 1 sales.db

--- practice-query-1-salesDb.sql
+++ practice-query-1-salesDb.sql
@@ -13,6 +13,10 @@ SELECT * FROM Customers
 	WHERE city = 'cleveland' AND Sales >= '50000' 
 	order by Sales;
 
+select * From Customers 
+	where City = 'cincinnati' and Sales > 30000 
+	order by Sales;
+
 SELECT * FROM OrderLines;
 
 SELECT * FROM Orders;
