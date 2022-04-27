bfa8c6b5efc99166b67d7120d17d22270d47b104
John McGaughey <JMcGaughey@tql.com>
2022-04-27 11:59:37 -0400

    deleted EdDb.sql (incorrect file), additions to practice-query-1-salesDB.sql

--- practice-query-1-salesDb.sql
+++ practice-query-1-salesDb.sql
@@ -14,8 +14,21 @@ SELECT * FROM Customers
 	order by Sales;
 
 select * From Customers 
-	where City = 'cincinnati' and Sales > 30000 
-	order by Sales;
+	where (City = 'cincinnati' and Sales > 30000)
+	or (city = 'columbus' and Sales > 35000)
+	or (City = 'cleveland' and Sales < 10000);
+
+select * from Customers
+	where city != 'cincinnati'; 
+--  where not (city = 'cincinnati';
+--  either way works for SQL, != more recognized in programming
+
+select * from Customers
+	where not (City = 'cleveland' or City = 'columbus');
+
+-- same as city = 'cleveland' or city = 'columbus
+select * from Customers
+	where City in ('cleveland','columbus');
 
 SELECT * FROM OrderLines;
 
